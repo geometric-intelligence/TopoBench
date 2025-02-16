d561b1302d0fa0c637b437d74deb08dee15b38d8
levtelyatnikov <tlscabinet@gmail.com>
2025-02-16 02:58:52 -0800

    adding one_hot

--- scripts/TopoSANN/GPSE_cellular/gpse_zinc.sh
+++ scripts/TopoSANN/GPSE_cellular/gpse_zinc.sh
@@ -1,5 +1,5 @@
 dataset='ZINC'
-project_name="TBX_GPSE_CELLTEST_$dataset"
+project_name="TBX_GPSE_CELLTEST3_$dataset"
 
 # =====================
 # DATA
@@ -22,7 +22,7 @@ WEIGHT_DECAYS=(0 0.0001)
 # =====================
 # PRETRAINED MODELS
 # =====================
-PRETRAIN_MODELS=('ZINC' 'PCQM4MV2')
+PRETRAIN_MODELS=('ZINC' 'GEOM' 'MOLPCBA' 'PCQM4MV2')
 
 
 # =====================
@@ -40,6 +40,7 @@ PRETRAIN_MODELS_STR=$(IFS=,; echo "${PRETRAIN_MODELS[*]}")  # Convert to comma-s
 # PARAMETERS OVER WHICH WE PERFORM PARALLEL RUNS
 # =====================
 batch_sizes=(128 256)
+learning_rates=(0.01 0.001)
 neighborhoods=(
     # adjacency 
     "['up_adjacency-0']"
@@ -58,36 +59,39 @@ neighborhoods=(
 )
 
 # TODO: fix bug with transforms.one_hot_node_degree_features.degrees_fields=x\
-gpus=(0 1 2 3 4 5 6 7 8)
-for i in {0..7}; do #0..1
+gpus=(0 1 2 3 4 5 6)
+for i in {0..6}; do #0..1
     CUDA=${gpus[$i]}  # Use the GPU number from our gpus array
     neighborhood=${neighborhoods[$i]} # Use the neighbourhood from our neighbourhoods array
 
     for batch_size in ${batch_sizes[*]}
     do
-    python topobenchmarkx/run.py\
-        dataset=graph/$dataset\
-        model=cell/sann\
-        model.backbone.n_layers=$N_LAYERS_STR\
-        model.feature_encoder.out_channels=$OUT_CHANNELS_STR\
-        model.feature_encoder.proj_dropout=$PROJECTION_DROPOUTS_STR\
-        dataset.split_params.data_seed=$DATA_SEEDS_STR\
-        dataset.dataloader_params.batch_size=$batch_size\
-        trainer.max_epochs=500\
-        trainer.min_epochs=50\
-        trainer.devices=\[$CUDA\]\
-        trainer.check_val_every_n_epoch=5\
-        logger.wandb.project=$project_name\
-        optimizer.parameters.lr=$LEARNING_RATES_STR\
-        optimizer.parameters.weight_decay=$WEIGHT_DECAYS_STR\
-        callbacks.early_stopping.patience=10\
-        transforms.graph2cell_lifting.max_cell_length=10\
-        transforms/data_manipulations@transforms.sann_encoding=add_gpse_information\
-        transforms.sann_encoding.pretrain_model=$PRETRAIN_MODELS_STR\
-        transforms.sann_encoding.copy_initial=True \
-        transforms.sann_encoding.neighborhoods=$neighborhood\
-        transforms.graph2cell_lifting.neighborhoods=$neighborhood\
-        --multirun &
+        for lr in ${learning_rates[*]}
+        do
+            python topobenchmarkx/run.py\
+                dataset=graph/$dataset\
+                model=cell/sann\
+                model.backbone.n_layers=$N_LAYERS_STR\
+                model.feature_encoder.out_channels=$OUT_CHANNELS_STR\
+                model.feature_encoder.proj_dropout=$PROJECTION_DROPOUTS_STR\
+                dataset.split_params.data_seed=$DATA_SEEDS_STR\
+                dataset.dataloader_params.batch_size=$batch_size\
+                trainer.max_epochs=500\
+                trainer.min_epochs=50\
+                trainer.devices=\[$CUDA\]\
+                trainer.check_val_every_n_epoch=5\
+                logger.wandb.project=$project_name\
+                optimizer.parameters.lr=$lr\
+                optimizer.parameters.weight_decay=$WEIGHT_DECAYS_STR\
+                callbacks.early_stopping.patience=10\
+                transforms=ZINC_sann_experiment \
+                transforms.graph2cell_lifting.max_cell_length=10\
+                transforms/data_manipulations@transforms.sann_encoding=add_gpse_information\
+                transforms.sann_encoding.pretrain_model=$PRETRAIN_MODELS_STR\
+                transforms.sann_encoding.copy_initial=True \
+                transforms.sann_encoding.neighborhoods=$neighborhood\
+                transforms.graph2cell_lifting.neighborhoods=$neighborhood\
+                --multirun &
+        done
     done
 done
-
