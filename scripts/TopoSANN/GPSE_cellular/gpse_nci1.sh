ce7040a22181692abc36e3a7dafb53c8536cf991
levtelyatnikov <tlscabinet@gmail.com>
2025-02-25 01:31:37 -0800

    epdated nci script

--- scripts/TopoSANN/GPSE_cellular/gpse_nci1.sh
+++ scripts/TopoSANN/GPSE_cellular/gpse_nci1.sh
@@ -17,10 +17,10 @@ OUT_CHANNELS=(128 256)
 # =====================
 # OPTIMIZATION PARAMETERS
 # =====================
-# LEARNING_RATES=(0.001)
+LEARNING_RATES=(0.01 0.001)
 PROJECTION_DROPOUTS=(0.25 0.5)
 WEIGHT_DECAYS=(0 0.0001)
-
+BATCH_SIZES=(128 256)
 # =====================
 # PRETRAINED MODELS
 # =====================
@@ -33,16 +33,17 @@ PRETRAIN_MODELS=('ZINC' 'GEOM' 'MOLPCBA' 'PCQM4MV2')
 DATA_SEEDS_STR=$(IFS=,; echo "${DATA_SEEDS[*]}")  # Convert to comma-separated string
 N_LAYERS_STR=$(IFS=,; echo "${N_LAYERS[*]}")  # Convert to comma-separated string
 OUT_CHANNELS_STR=$(IFS=,; echo "${OUT_CHANNELS[*]}")  # Convert to comma-separated string
-# LEARNING_RATES_STR=$(IFS=,; echo "${LEARNING_RATES[*]}")  # Convert to comma-separated string
+LEARNING_RATES_STR=$(IFS=,; echo "${LEARNING_RATES[*]}")  # Convert to comma-separated string
 PROJECTION_DROPOUTS_STR=$(IFS=,; echo "${PROJECTION_DROPOUTS[*]}")  # Convert to comma-separated string
 WEIGHT_DECAYS_STR=$(IFS=,; echo "${WEIGHT_DECAYS[*]}")  # Convert to comma-separated string
 PRETRAIN_MODELS_STR=$(IFS=,; echo "${PRETRAIN_MODELS[*]}")  # Convert to comma-separated string
+BATCH_SIZES_STR=$(IFS=,; echo "${BATCH_SIZES[*]}")
 
 # =====================
 # PARAMETERS OVER WHICH WE PERFORM PARALLEL RUNS
 # =====================
-batch_sizes=(128 256)
-learning_rates=(0.01 0.001)
+# batch_sizes=(128 256)
+# learning_rates=(0.01 0.001)
 neighborhoods=(
     # adjacency 
     "['up_adjacency-0']"
@@ -66,32 +67,31 @@ for i in {0..7}; do
     CUDA=${gpus[$i]}  # Use the GPU number from our gpus array
     neighborhood=${neighborhoods[$i]} # Use the neighbourhood from our neighbourhoods array
 
-    for batch_size in ${batch_sizes[*]}
+    for pretrain_model in ${PRETRAIN_MODELS[*]}
     do
-        for lr in ${learning_rates[*]}
-        do
-            python topobenchmarkx/run.py\
-                dataset=graph/$dataset\
-                model=cell/sann\
-                model.backbone.n_layers=$N_LAYERS_STR\
-                model.feature_encoder.out_channels=$OUT_CHANNELS_STR\
-                model.feature_encoder.proj_dropout=$PROJECTION_DROPOUTS_STR\
-                dataset.split_params.data_seed=$DATA_SEEDS_STR\
-                dataset.dataloader_params.batch_size=$batch_size\
-                trainer.max_epochs=500\
-                trainer.min_epochs=50\
-                trainer.devices=\[$CUDA\]\
-                trainer.check_val_every_n_epoch=5\
-                logger.wandb.project=$project_name\
-                optimizer.parameters.lr=$lr\
-                optimizer.parameters.weight_decay=$WEIGHT_DECAYS_STR\
-                callbacks.early_stopping.patience=10\
-                transforms/data_manipulations@transforms.sann_encoding=add_gpse_information\
-                transforms.sann_encoding.pretrain_model=$PRETRAIN_MODELS_STR\
-                transforms.sann_encoding.copy_initial=True \
-                transforms.sann_encoding.neighborhoods=$neighborhood\
-                transforms.graph2cell_lifting.neighborhoods=$neighborhood\
-                --multirun &
-        done
+        
+        python topobenchmark/run.py\
+            dataset=graph/$dataset\
+            model=cell/sann\
+            model.backbone.n_layers=$N_LAYERS_STR\
+            model.feature_encoder.out_channels=$OUT_CHANNELS_STR\
+            model.feature_encoder.proj_dropout=$PROJECTION_DROPOUTS_STR\
+            dataset.split_params.data_seed=$DATA_SEEDS_STR\
+            dataset.dataloader_params.batch_size=$BATCH_SIZES_STR\
+            trainer.max_epochs=500\
+            trainer.min_epochs=50\
+            trainer.devices=\[$CUDA\]\
+            trainer.check_val_every_n_epoch=5\
+            logger.wandb.project=$project_name\
+            optimizer.parameters.lr=$LEARNING_RATES_STR\
+            optimizer.parameters.weight_decay=$WEIGHT_DECAYS_STR\
+            callbacks.early_stopping.patience=10\
+            transforms/data_manipulations@transforms.sann_encoding=add_gpse_information\
+            transforms.sann_encoding.pretrain_model=$pretrain_model\
+            transforms.sann_encoding.copy_initial=True \
+            transforms.sann_encoding.neighborhoods=$neighborhood\
+            transforms.graph2cell_lifting.neighborhoods=$neighborhood\
+            --multirun &
+        
     done
 done
--- scripts/TopoSANN/GPSE_cellular/gpse_zinc.sh
+++ scripts/TopoSANN/GPSE_cellular/gpse_zinc.sh
@@ -15,7 +15,7 @@ OUT_CHANNELS=(128 256)
 # =====================
 # OPTIMIZATION PARAMETERS
 # =====================
-# LEARNING_RATES=(0.001)
+LEARNING_RATES=(0.001)
 PROJECTION_DROPOUTS=(0.25 0.5)
 WEIGHT_DECAYS=(0 0.0001)
 
@@ -31,7 +31,7 @@ PRETRAIN_MODELS=('ZINC' 'GEOM' 'MOLPCBA' 'PCQM4MV2')
 DATA_SEEDS_STR=$(IFS=,; echo "${DATA_SEEDS[*]}")  # Convert to comma-separated string
 N_LAYERS_STR=$(IFS=,; echo "${N_LAYERS[*]}")  # Convert to comma-separated string
 OUT_CHANNELS_STR=$(IFS=,; echo "${OUT_CHANNELS[*]}")  # Convert to comma-separated string
-# LEARNING_RATES_STR=$(IFS=,; echo "${LEARNING_RATES[*]}")  # Convert to comma-separated string
+LEARNING_RATES_STR=$(IFS=,; echo "${LEARNING_RATES[*]}")  # Convert to comma-separated string
 PROJECTION_DROPOUTS_STR=$(IFS=,; echo "${PROJECTION_DROPOUTS[*]}")  # Convert to comma-separated string
 WEIGHT_DECAYS_STR=$(IFS=,; echo "${WEIGHT_DECAYS[*]}")  # Convert to comma-separated string
 PRETRAIN_MODELS_STR=$(IFS=,; echo "${PRETRAIN_MODELS[*]}")  # Convert to comma-separated string
@@ -68,14 +68,14 @@ for i in {0..7}; do
 
     for batch_size in ${batch_sizes[*]}
     do
-        for lr in ${learning_rates[*]}
+        for pd in ${PROJECTION_DROPOUTS[*]}
         do
-            python topobenchmarkx/run.py\
+            python topobenchmark/run.py\
                 dataset=graph/$dataset\
                 model=cell/sann\
                 model.backbone.n_layers=$N_LAYERS_STR\
                 model.feature_encoder.out_channels=$OUT_CHANNELS_STR\
-                model.feature_encoder.proj_dropout=$PROJECTION_DROPOUTS_STR\
+                model.feature_encoder.proj_dropout=$pd\
                 dataset.split_params.data_seed=$DATA_SEEDS_STR\
                 dataset.dataloader_params.batch_size=$batch_size\
                 trainer.max_epochs=500\
@@ -83,7 +83,7 @@ for i in {0..7}; do
                 trainer.devices=\[$CUDA\]\
                 trainer.check_val_every_n_epoch=5\
                 logger.wandb.project=$project_name\
-                optimizer.parameters.lr=$lr\
+                optimizer.parameters.lr=$LEARNING_RATES_STR\
                 optimizer.parameters.weight_decay=$WEIGHT_DECAYS_STR\
                 callbacks.early_stopping.patience=10\
                 transforms=ZINC_sann_experiment \
