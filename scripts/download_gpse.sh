0fe8c11fed396e0c4870edea1c6b0cfbbadf6518
levtelyatnikov <tlscabinet@gmail.com>
2025-02-27 01:10:29 -0800

    workedout ckpt

--- scripts/download_gpse.sh
+++ scripts/download_gpse.sh
@@ -1,7 +1,8 @@
 # Train rest of the TU graph datasets
 if [ ! -d "data/pretrained_models" ]; then
-    mkdir data/pretrained_models
+    mkdir -p data/pretrained_models
 fi
+
 if [ ! -f "data/pretrained_models/gpse_zinc.pt" ]; then
     wget https://zenodo.org/record/8145095/files/gpse_model_zinc_1.0.pt -O data/pretrained_models/gpse_zinc.pt
 fi
