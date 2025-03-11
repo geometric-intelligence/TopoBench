175b4c2eb2af0ec30e203a4ed96f6d05473a9df5
levtelyatnikov <tlscabinet@gmail.com>
2025-03-11 01:42:03 -0700

    scripts running

--- scripts/TopoSANN/GPSE/gpse_zinc.sh
+++ scripts/TopoSANN/GPSE/gpse_zinc.sh
@@ -67,6 +67,44 @@ for i in {0..7}; do
     CUDA=${gpus[$i]}  # Use the GPU number from our gpus array
     neighborhood=${neighborhoods[$i]} # Use the neighbourhood from our neighbourhoods array
 
+    for pret_model in ${PRETRAIN_MODELS[*]}
+    do
+        python topobenchmark/run.py\
+            dataset=graph/$dataset\
+            model=simplicial/sann\
+            model.backbone.n_layers=1\
+            model.feature_encoder.out_channels=128\
+            model.feature_encoder.proj_dropout=0.25\
+            dataset.split_params.data_seed=0\
+            dataset.dataloader_params.batch_size=128\
+            trainer.max_epochs=5\
+            trainer.min_epochs=1\
+            trainer.devices=\[$CUDA\]\
+            trainer.check_val_every_n_epoch=1\
+            logger.wandb.project='prerun'\
+            optimizer.parameters.lr=0.01\
+            optimizer.parameters.weight_decay=0.25\
+            callbacks.early_stopping.patience=10\
+            transforms=ZINC_sann_experiment_simplicial\
+            transforms.graph2cell_lifting.max_cell_length=10\
+            transforms/data_manipulations@transforms.sann_encoding=add_gpse_information\
+            transforms.sann_encoding.pretrain_model=$pret_model\
+            transforms.sann_encoding.copy_initial=True \
+            transforms.sann_encoding.neighborhoods=$neighborhood\
+            transforms.graph2cell_lifting.neighborhoods=$neighborhood\
+            --multirun &
+            wait
+    done
+    wait
+done
+wait
+
+# TODO: fix bug with transforms.one_hot_node_degree_features.degrees_fields=x\
+gpus=(0 1 2 3 4 5 6 7)
+for i in {0..7}; do 
+    CUDA=${gpus[$i]}  # Use the GPU number from our gpus array
+    neighborhood=${neighborhoods[$i]} # Use the neighbourhood from our neighbourhoods array
+
     for batch_size in ${batch_sizes[*]}
     do
         for pd in ${PROJECTION_DROPOUTS[*]}
--- scripts/TopoSANN/GPSE_cellular/gpse_mutag.sh
+++ scripts/TopoSANN/GPSE_cellular/gpse_mutag.sh
@@ -64,6 +64,39 @@ for i in {0..7}; do
     CUDA=${gpus[$i]}  # Use the GPU number from our gpus array
     neighborhood=${neighborhoods[$i]} # Use the neighbourhood from our neighbourhoods array
 
+    for pret_model in ${PRETRAIN_MODELS[*]}
+    do
+        python topobenchmark/run.py\
+            dataset=graph/$dataset\
+            model=cell/sann\
+            model.backbone.n_layers=1\
+            model.feature_encoder.out_channels=128\
+            model.feature_encoder.proj_dropout=0.25\
+            dataset.split_params.data_seed=0\
+            dataset.dataloader_params.batch_size=128\
+            trainer.max_epochs=5\
+            trainer.min_epochs=1\
+            trainer.devices=\[$CUDA\]\
+            trainer.check_val_every_n_epoch=1\
+            logger.wandb.project='prerun'\
+            optimizer.parameters.lr=0.01\
+            optimizer.parameters.weight_decay=0.25\
+            callbacks.early_stopping.patience=10\
+            transforms=MUTAG_sann_experiment\
+            transforms/data_manipulations@transforms.sann_encoding=add_gpse_information\
+            transforms.sann_encoding.pretrain_model=$pret_model\
+            transforms.sann_encoding.copy_initial=True \
+            transforms.sann_encoding.neighborhoods=$neighborhood\
+            transforms.graph2cell_lifting.neighborhoods=$neighborhood\
+            --multirun &
+    done
+done
+wait
+
+for i in {0..7}; do 
+    CUDA=${gpus[$i]}  # Use the GPU number from our gpus array
+    neighborhood=${neighborhoods[$i]} # Use the neighbourhood from our neighbourhoods array
+
     for batch_size in ${batch_sizes[*]}
     do
         for lr in ${learning_rates[*]}
