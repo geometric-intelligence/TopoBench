9da458e2ab75706fde7ba92f06576b6eb33d530a
levtelyatnikov <tlscabinet@gmail.com>
2025-03-31 11:01:41 -0700

    mantra

--- scripts/TopoSANN/HOPSE_simplicial/mantra_orientation.sh
+++ scripts/TopoSANN/HOPSE_simplicial/mantra_orientation.sh
@@ -1,5 +1,5 @@
-dataset='PROTEINS'
-project_name="HOPSE_cell_$dataset"
+dataset='mantra_orientation'
+project_name="HOPSE_simplicial_$dataset"
 
 # =====================
 # DATA
@@ -16,15 +16,15 @@ OUT_CHANNELS=(128 256)
 # OPTIMIZATION PARAMETERS
 # =====================
 LEARNING_RATES=(0.01 0.001)
-PROJECTION_DROPOUTS=(0.25 0.5)
+PROJECTION_DROPOUTS=(0.25)
 WEIGHT_DECAYS=(0 0.0001)
+BATCH_SIZES=(256)
 
 # =====================
 # PRETRAINED MODELS
 # =====================
 # PRETRAIN_MODELS=('ZINC' 'GEOM' 'MOLPCBA' 'PCQM4MV2')
 
-# seed=42,3,5,23,150
 # =====================
 # CONVERT TO STRINGS
 # =====================
@@ -35,11 +35,11 @@ LEARNING_RATES_STR=$(IFS=,; echo "${LEARNING_RATES[*]}")  # Convert to comma-sep
 PROJECTION_DROPOUTS_STR=$(IFS=,; echo "${PROJECTION_DROPOUTS[*]}")  # Convert to comma-separated string
 WEIGHT_DECAYS_STR=$(IFS=,; echo "${WEIGHT_DECAYS[*]}")  # Convert to comma-separated string
 PRETRAIN_MODELS_STR=$(IFS=,; echo "${PRETRAIN_MODELS[*]}")  # Convert to comma-separated string
+BATCH_SIZES_STR=$(IFS=,; echo "${BATCH_SIZES[*]}")
 
 # =====================
 # PARAMETERS OVER WHICH WE PERFORM PARALLEL RUNS
 # =====================
-batch_sizes=(128 256)
 neighborhoods=(
     # adjacency 
     "['up_adjacency-0']"
@@ -67,8 +67,8 @@ for i in {0..7}; do
 
     
     python topobench/run.py\
-        dataset=graph/$dataset\
-        model=cell/sann\
+        dataset=simplicial/$dataset\
+        model=simplicial/sann\
         model.backbone.n_layers=1\
         model.feature_encoder.out_channels=128\
         model.feature_encoder.proj_dropout=0.25\
@@ -82,12 +82,11 @@ for i in {0..7}; do
         optimizer.parameters.lr=0.01\
         optimizer.parameters.weight_decay=0.25\
         callbacks.early_stopping.patience=10\
-        transforms=HOPSE_PS_experiment_cell\
-        transforms.graph2cell_lifting.max_cell_length=10\
+        transforms=HOPSE_PS_experiment_MANTRA\
         transforms.sann_encoding.neighborhoods=$neighborhood\
-        transforms.graph2cell_lifting.neighborhoods=$neighborhood\
+        transforms.redefine_simplicial_neighbourhoods.neighborhoods=$neighborhood\
         --multirun &
-        sleep 10
+        sleep 5
 done
 wait
 
@@ -96,30 +95,29 @@ for i in {0..7}; do
     CUDA=${gpus[$i]}  # Use the GPU number from our gpus array
     neighborhood=${neighborhoods[$i]} # Use the neighbourhood from our neighbourhoods array
 
-    for batch_size in ${batch_sizes[*]}
+    for lr in ${LEARNING_RATES[*]}
     do
-        for pd in ${PROJECTION_DROPOUTS[*]}
+        for wd in ${WEIGHT_DECAYS[*]}
         do
             python topobench/run.py\
-                dataset=graph/$dataset\
-                model=cell/sann\
+                dataset=simplicial/$dataset\
+                model=simplicial/sann\
                 model.backbone.n_layers=$N_LAYERS_STR\
                 model.feature_encoder.out_channels=$OUT_CHANNELS_STR\
-                model.feature_encoder.proj_dropout=$pd\
+                model.feature_encoder.proj_dropout=$PROJECTION_DROPOUTS_STR\
                 dataset.split_params.data_seed=$DATA_SEEDS_STR\
-                dataset.dataloader_params.batch_size=$batch_size\
+                dataset.dataloader_params.batch_size=$BATCH_SIZES_STR\
                 trainer.max_epochs=500\
                 trainer.min_epochs=50\
                 trainer.devices=\[$CUDA\]\
                 trainer.check_val_every_n_epoch=5\
                 logger.wandb.project=$project_name\
-                optimizer.parameters.lr=$LEARNING_RATES_STR\
-                optimizer.parameters.weight_decay=$WEIGHT_DECAYS_STR\
+                optimizer.parameters.lr=$lr\
+                optimizer.parameters.weight_decay=$wd\
                 callbacks.early_stopping.patience=10\
-                transforms=HOPSE_PS_experiment_cell\
-                transforms.graph2cell_lifting.max_cell_length=10\
+                transforms=HOPSE_PS_experiment_MANTRA\
                 transforms.sann_encoding.neighborhoods=$neighborhood\
-                transforms.graph2cell_lifting.neighborhoods=$neighborhood\
+                transforms.redefine_simplicial_neighbourhoods.neighborhoods=$neighborhood\
                 --multirun &
         done
     done
