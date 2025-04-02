1e806ed9795deaa891e05b8131976f0e17eb2aff
levtelyatnikov <tlscabinet@gmail.com>
2025-04-02 09:45:15 -0700

    some mantra

--- scripts/TopoSANN/GPSE_simplicial/mantra.sh
+++ scripts/TopoSANN/GPSE_simplicial/mantra.sh
@@ -1,6 +1,3 @@
-dataset='roman_tolokers'
-project_name="GPSE_cell_$dataset"
-
 # =====================
 # DATA
 # =====================
@@ -16,14 +13,14 @@ OUT_CHANNELS=(128 256)
 # OPTIMIZATION PARAMETERS
 # =====================
 LEARNING_RATES=(0.01 0.001)
-PROJECTION_DROPOUTS=(0.25 0.5)
+PROJECTION_DROPOUTS=(0.25)
 WEIGHT_DECAYS=(0 0.0001)
+BATCH_SIZES=(256)
 
 # =====================
 # PRETRAINED MODELS
 # =====================
-PRETRAIN_MODELS=('ZINC' 'PCQM4MV2') # 'PCQM4MV2' 'GEOM' 'MOLPCBA' # 'PCQM4MV2' 'GEOM' 'MOLPCBA'
-
+# PRETRAIN_MODELS=('ZINC' 'GEOM' 'MOLPCBA' 'PCQM4MV2')
 
 # =====================
 # CONVERT TO STRINGS
@@ -35,13 +32,11 @@ LEARNING_RATES_STR=$(IFS=,; echo "${LEARNING_RATES[*]}")  # Convert to comma-sep
 PROJECTION_DROPOUTS_STR=$(IFS=,; echo "${PROJECTION_DROPOUTS[*]}")  # Convert to comma-separated string
 WEIGHT_DECAYS_STR=$(IFS=,; echo "${WEIGHT_DECAYS[*]}")  # Convert to comma-separated string
 PRETRAIN_MODELS_STR=$(IFS=,; echo "${PRETRAIN_MODELS[*]}")  # Convert to comma-separated string
-
+BATCH_SIZES_STR=$(IFS=,; echo "${BATCH_SIZES[*]}")
 
 # =====================
 # PARAMETERS OVER WHICH WE PERFORM PARALLEL RUNS
 # =====================
-# batch_sizes=(128 256)
-# learning_rates=(0.01 0.001)
 neighborhoods=(
     # adjacency 
     "['up_adjacency-0']"
@@ -58,19 +53,23 @@ neighborhoods=(
     
     # We have 8th gpu hence we can add one more neighbourhood
     "['up_adjacency-0','up_adjacency-1','2-up_adjacency-0','down_adjacency-1','down_adjacency-2','2-down_adjacency-2']"
+
 )
 
-
-gpus=(0 1 2 3 4 5 6 7)
-for i in {0..7}; do 
-    CUDA=${gpus[$i]}  # Use the GPU number from our gpus array
-    neighborhood=${neighborhoods[$i]} # Use the neighbourhood from our neighbourhoods array
-
-    for pret_model in ${PRETRAIN_MODELS[*]}
+datasets=('mantra_name' 'mantra_orientation')
+for dataset in ${datasets[*]}
     do
+    project_name="GPSE_simplicial_$dataset"
+
+
+    gpus=(0 1 2 3 4 5 6 7)
+    for i in {0..7}; do 
+        CUDA=${gpus[$i]}  # Use the GPU number from our gpus array
+        neighborhood=${neighborhoods[$i]} # Use the neighbourhood from our neighbourhoods array
+        
         python topobench/run.py\
-            dataset=graph/$dataset\
-            model=cell/sann\
+            dataset=simplicial/$dataset\
+            model=simplicial/sann\
             model.backbone.n_layers=1\
             model.feature_encoder.out_channels=128\
             model.feature_encoder.proj_dropout=0.25\
@@ -84,47 +83,44 @@ for i in {0..7}; do
             optimizer.parameters.lr=0.01\
             optimizer.parameters.weight_decay=0.25\
             callbacks.early_stopping.patience=10\
-            transforms/data_manipulations@transforms.sann_encoding=add_gpse_information\
-            transforms.sann_encoding.pretrain_model=$pret_model\
-            transforms.sann_encoding.copy_initial=True \
+            transforms=GPSE_MANTRA\
             transforms.sann_encoding.neighborhoods=$neighborhood\
-            transforms.graph2cell_lifting.neighborhoods=$neighborhood\
+            transforms.redefine_simplicial_neighbourhoods.neighborhoods=$neighborhood\
             --multirun &
+            sleep 5
+    done
+    wait
+
+    for i in {0..7}; do 
+        CUDA=${gpus[$i]}  # Use the GPU number from our gpus array
+        neighborhood=${neighborhoods[$i]} # Use the neighbourhood from our neighbourhoods array
+
+        for lr in ${LEARNING_RATES[*]}
+        do
+            for wd in ${WEIGHT_DECAYS[*]}
+            do
+                python topobench/run.py\
+                    dataset=simplicial/$dataset\
+                    model=simplicial/sann\
+                    model.backbone.n_layers=$N_LAYERS_STR\
+                    model.feature_encoder.out_channels=$OUT_CHANNELS_STR\
+                    model.feature_encoder.proj_dropout=$PROJECTION_DROPOUTS_STR\
+                    dataset.split_params.data_seed=$DATA_SEEDS_STR\
+                    dataset.dataloader_params.batch_size=$BATCH_SIZES_STR\
+                    trainer.max_epochs=500\
+                    trainer.min_epochs=50\
+                    trainer.devices=\[$CUDA\]\
+                    trainer.check_val_every_n_epoch=5\
+                    logger.wandb.project=$project_name\
+                    optimizer.parameters.lr=$lr\
+                    optimizer.parameters.weight_decay=$wd\
+                    callbacks.early_stopping.patience=10\
+                    transforms=GPSE_MANTRA\
+                    transforms.sann_encoding.neighborhoods=$neighborhood\
+                    transforms.redefine_simplicial_neighbourhoods.neighborhoods=$neighborhood\
+                    --multirun &
+            done
+        done
     done
     wait
 done
-wait
-
-for i in {0..7}; do 
-    CUDA=${gpus[$i]}  # Use the GPU number from our gpus array
-    neighborhood=${neighborhoods[$i]} # Use the neighbourhood from our neighbourhoods array
-
-    for pretrain_model in ${PRETRAIN_MODELS[*]}
-    do
-        
-        python topobench/run.py\
-            dataset=graph/$dataset\
-            model=cell/sann\
-            model.backbone.n_layers=$N_LAYERS_STR\
-            model.feature_encoder.out_channels=$OUT_CHANNELS_STR\
-            model.feature_encoder.proj_dropout=$PROJECTION_DROPOUTS_STR\
-            dataset.split_params.data_seed=$DATA_SEEDS_STR\
-            dataset.dataloader_params.batch_size="full"\
-            trainer.max_epochs=1000\
-            trainer.min_epochs=50\
-            trainer.devices=\[$CUDA\]\
-            trainer.check_val_every_n_epoch=5\
-            logger.wandb.project=$project_name\
-            optimizer.parameters.lr=$LEARNING_RATES_STR\
-            optimizer.parameters.weight_decay=$WEIGHT_DECAYS_STR\
-            callbacks.early_stopping.patience=10\
-            transforms/data_manipulations@transforms.sann_encoding=add_gpse_information\
-            transforms.sann_encoding.pretrain_model=$pretrain_model\
-            transforms.sann_encoding.copy_initial=True \
-            transforms.sann_encoding.neighborhoods=$neighborhood\
-            transforms.graph2cell_lifting.neighborhoods=$neighborhood\
-            --multirun &
-        
-    done
-done
-wait
