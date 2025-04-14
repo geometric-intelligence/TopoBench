9a6b3999034319a3dc0f7efcea85a4dfab0fcfd3
levtelyatnikov <tlscabinet@gmail.com>
2025-04-14 08:01:23 -0700

    added barycentric subdivision transform

--- scripts/TopoSANN/GPSE_cellular/gpse_reddit_b.sh
+++ scripts/TopoSANN/GPSE_cellular/gpse_reddit_b.sh
@@ -64,38 +64,37 @@ neighborhoods=(
 
 
 gpus=(0 1 2 3 4 5 6 7)
-# for i in {0..7}; do 
-#     CUDA=${gpus[$i]}  # Use the GPU number from our gpus array
-#     neighborhood=${neighborhoods[$i]} # Use the neighbourhood from our neighbourhoods array
+for i in {0..7}; do 
+    CUDA=${gpus[$i]}  # Use the GPU number from our gpus array
+    neighborhood=${neighborhoods[$i]} # Use the neighbourhood from our neighbourhoods array
 
-#     for pret_model in ${PRETRAIN_MODELS[*]}
-#     do
-#         python topobench/run.py\
-#             dataset=graph/$dataset\
-#             model=cell/sann\
-#             model.backbone.n_layers=4\
-#             model.feature_encoder.out_channels=64\
-#             model.feature_encoder.proj_dropout=0.25\
-#             dataset.split_params.data_seed=0\
-#             dataset.dataloader_params.batch_size=32\
-#             trainer.max_epochs=5\
-#             trainer.min_epochs=1\
-#             trainer.devices=\[$CUDA\]\
-#             trainer.check_val_every_n_epoch=1\
-#             logger.wandb.project='prerun'\
-#             optimizer.parameters.lr=0.01\
-#             optimizer.parameters.weight_decay=0.25\
-#             callbacks.early_stopping.patience=10\
-#             transforms=GPSE_REDDIT_cell\
-#             transforms.sann_encoding.pretrain_model=$pret_model\
-#             transforms.sann_encoding.copy_initial=True \
-#             transforms.sann_encoding.neighborhoods=$neighborhood\
-#             transforms.graph2cell_lifting.neighborhoods=$neighborhood\
-#             --multirun &
-#     done
-#     wait
-# done
-# wait
+    for pret_model in ${PRETRAIN_MODELS[*]}
+    do
+        python topobench/run.py\
+            dataset=graph/$dataset\
+            model=cell/sann\
+            model.backbone.n_layers=1\
+            model.feature_encoder.out_channels=8\
+            model.feature_encoder.proj_dropout=0.25\
+            dataset.split_params.data_seed=0\
+            dataset.dataloader_params.batch_size=16\
+            trainer.max_epochs=5\
+            trainer.min_epochs=1\
+            trainer.devices=\[$CUDA\]\
+            trainer.check_val_every_n_epoch=1\
+            logger.wandb.project='prerun'\
+            optimizer.parameters.lr=0.01\
+            optimizer.parameters.weight_decay=0.25\
+            callbacks.early_stopping.patience=10\
+            transforms=GPSE_REDDIT_cell\
+            transforms.sann_encoding.pretrain_model=$pret_model\
+            transforms.sann_encoding.copy_initial=True \
+            transforms.sann_encoding.neighborhoods=$neighborhood\
+            transforms.graph2cell_lifting.neighborhoods=$neighborhood\
+            --multirun &
+    done
+done
+wait
 
 PRETRAIN_MODELS=('ZINC' 'PCQM4MV2')
 gpus=(0 1 2 3 4 5 6 7)
