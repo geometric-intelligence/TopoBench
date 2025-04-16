e8d1c045ac08fb9449d80e353b9bc62859adfe58
levtelyatnikov <tlscabinet@gmail.com>
2025-04-16 01:26:43 -0700

    some

--- scripts/TopoSANN/GPSE_cellular/gpse_reddit_b.sh
+++ scripts/TopoSANN/GPSE_cellular/gpse_reddit_b.sh
@@ -24,7 +24,7 @@ BATCH_SIZES=(32 64)
 # =====================
 # PRETRAINED MODELS
 # =====================
-PRETRAIN_MODELS=('ZINC' 'PCQM4MV2' 'GEOM' 'MOLPCBA') # 
+PRETRAIN_MODELS=('ZINC') #  'PCQM4MV2' 'GEOM' 'MOLPCBA'
 
 
 # =====================
@@ -64,74 +64,39 @@ neighborhoods=(
 
 
 gpus=(0 1 2 3 4 5 6 7)
-for i in {0..7}; do 
-    CUDA=${gpus[$i]}  # Use the GPU number from our gpus array
-    neighborhood=${neighborhoods[$i]} # Use the neighbourhood from our neighbourhoods array
+# for i in {0..7}; do 
+#     CUDA=${gpus[$i]}  # Use the GPU number from our gpus array
+#     neighborhood=${neighborhoods[$i]} # Use the neighbourhood from our neighbourhoods array
 
-    for pret_model in ${PRETRAIN_MODELS[*]}
-    do
-        python topobench/run.py\
-            dataset=graph/$dataset\
-            model=cell/sann\
-            model.backbone.n_layers=1\
-            model.feature_encoder.out_channels=8\
-            model.feature_encoder.proj_dropout=0.25\
-            dataset.split_params.data_seed=0\
-            dataset.dataloader_params.batch_size=16\
-            trainer.max_epochs=5\
-            trainer.min_epochs=1\
-            trainer.devices=\[$CUDA\]\
-            trainer.check_val_every_n_epoch=1\
-            logger.wandb.project='prerun'\
-            optimizer.parameters.lr=0.01\
-            optimizer.parameters.weight_decay=0.25\
-            callbacks.early_stopping.patience=10\
-            transforms=GPSE_REDDIT_cell\
-            transforms.sann_encoding.pretrain_model=$pret_model\
-            transforms.sann_encoding.copy_initial=True \
-            transforms.sann_encoding.neighborhoods=$neighborhood\
-            transforms.graph2cell_lifting.neighborhoods=$neighborhood\
-            --multirun &
-    done
-done
-wait
+#     for pret_model in ${PRETRAIN_MODELS[*]}
+#     do
+#         python topobench/run.py\
+#             dataset=graph/$dataset\
+#             model=cell/sann\
+#             model.backbone.n_layers=\
+#             model.feature_encoder.out_channels=8\
+#             model.feature_encoder.proj_dropout=0.25\
+#             dataset.split_params.data_seed=0\
+#             dataset.dataloader_params.batch_size=16\
+#             trainer.max_epochs=5\
+#             trainer.min_epochs=1\
+#             trainer.devices=\[$CUDA\]\
+#             trainer.check_val_every_n_epoch=1\
+#             logger.wandb.project='prerun'\
+#             optimizer.parameters.lr=0.01\
+#             optimizer.parameters.weight_decay=0.25\
+#             callbacks.early_stopping.patience=10\
+#             transforms=GPSE_REDDIT_cell\
+#             transforms.sann_encoding.pretrain_model=$pret_model\
+#             transforms.sann_encoding.copy_initial=True \
+#             transforms.sann_encoding.neighborhoods=$neighborhood\
+#             transforms.graph2cell_lifting.neighborhoods=$neighborhood\
+#             --multirun &
+#     done
+# done
+# wait
 
-PRETRAIN_MODELS=('ZINC' 'PCQM4MV2')
 gpus=(0 1 2 3 4 5 6 7)
-for i in {0..7}; do 
-    CUDA=${gpus[$i]}  # Use the GPU number from our gpus array
-    neighborhood=${neighborhoods[$i]} # Use the neighbourhood from our neighbourhoods array
-
-    for pretrain_model in ${PRETRAIN_MODELS[*]}
-    do
-        
-        python topobench/run.py\
-            dataset=graph/$dataset\
-            model=cell/sann\
-            model.backbone.n_layers=$N_LAYERS_STR\
-            model.feature_encoder.out_channels=$OUT_CHANNELS_STR\
-            model.feature_encoder.proj_dropout=$PROJECTION_DROPOUTS_STR\
-            dataset.split_params.data_seed=$DATA_SEEDS_STR\
-            dataset.dataloader_params.batch_size=$BATCH_SIZES_STR\
-            trainer.max_epochs=500\
-            trainer.min_epochs=50\
-            trainer.devices=\[$CUDA\]\
-            trainer.check_val_every_n_epoch=5\
-            logger.wandb.project=$project_name\
-            optimizer.parameters.lr=$LEARNING_RATES_STR\
-            optimizer.parameters.weight_decay=$WEIGHT_DECAYS_STR\
-            callbacks.early_stopping.patience=10\
-            transforms=GPSE_REDDIT_cell\
-            transforms.sann_encoding.pretrain_model=$pretrain_model\
-            transforms.sann_encoding.copy_initial=True \
-            transforms.sann_encoding.neighborhoods=$neighborhood\
-            transforms.graph2cell_lifting.neighborhoods=$neighborhood\
-            --multirun &
-    done
-done
-wait
-
-PRETRAIN_MODELS=('GEOM' 'MOLPCBA')
 for i in {0..7}; do 
     CUDA=${gpus[$i]}  # Use the GPU number from our gpus array
     neighborhood=${neighborhoods[$i]} # Use the neighbourhood from our neighbourhoods array
