17904041f232d7900fd617f2494dd2d9d0a693f5
levtelyatnikov <tlscabinet@gmail.com>
2025-07-29 02:55:45 -0700

    scripts

--- scripts/rebuttal/search/zinc.sh
+++ scripts/rebuttal/search/zinc.sh
@@ -48,69 +48,80 @@ neighborhoods=(
     "['up_adjacency-0','down_incidence-1']"
 )
 
-# TODO: fix bug with transforms.one_hot_node_degree_features.degrees_fields=x\
-gpus=(5 6 7)
-for i in {0..1}; do 
-    CUDA=${gpus[$i]}  # Use the GPU number from our gpus array
-    neighborhood=${neighborhoods[$i]} # Use the neighbourhood from our neighbourhoods array
 
-    
-    python topobench/run.py\
-        dataset=graph/$dataset\
-        model=graph/hopse_gin\
-        experiment=hopse_m_gnn_cell_zinc\
-        model.backbone.num_layers=1\
-        model.feature_encoder.out_channels=128\
-        model.feature_encoder.proj_dropout=0.25\
-        dataset.split_params.data_seed=0\
-        dataset.dataloader_params.batch_size=128\
-        trainer.max_epochs=5\
-        trainer.min_epochs=1\
-        trainer.devices=\[$CUDA\]\
-        trainer.check_val_every_n_epoch=1\
-        logger.wandb.project='prerun'\
-        optimizer.parameters.lr=0.01\
-        optimizer.parameters.weight_decay=0.25\
-        callbacks.early_stopping.patience=10\
-        transforms.graph2cell_lifting.max_cell_length=10\
-        transforms.sann_encoding.neighborhoods=$neighborhood\
-        transforms.graph2cell_lifting.neighborhoods=$neighborhood\
-        --multirun &
-        sleep 5
-done
-wait
-
-gpus=(5 6 7)
-for i in {0..1}; do 
-    CUDA=${gpus[$i]}  # Use the GPU number from our gpus array
-    neighborhood=${neighborhoods[$i]} # Use the neighbourhood from our neighbourhoods array
-
-    for batch_size in ${batch_sizes[*]}
+PE_TYPES=('RWSE' 'ElstaticPE' 'HKdiagSE' 'LapPE')
+model_types=('hopse_gin' 'hopse_gcn' 'hopse_gat')
+for model_type in ${model_types[*]}
+do
+    for pe_type in ${PE_TYPES[*]}
     do
-        for pd in ${PROJECTION_DROPOUTS[*]}
-        do
+        # TODO: fix bug with transforms.one_hot_node_degree_features.degrees_fields=x\
+        gpus=(3 4)
+        for i in {0..1}; do 
+            CUDA=${gpus[$i]}  # Use the GPU number from our gpus array
+            neighborhood=${neighborhoods[$i]} # Use the neighbourhood from our neighbourhoods array
+
+            
             python topobench/run.py\
                 dataset=graph/$dataset\
-                model=graph/hopse_gin\
+                model=graph/$model_type\
                 experiment=hopse_m_gnn_cell_zinc\
-                model.backbone.num_layers=$N_LAYERS_STR\
-                model.feature_encoder.out_channels=$OUT_CHANNELS_STR\
-                model.feature_encoder.proj_dropout=$pd\
-                seed=$DATA_SEEDS_STR\
-                dataset.dataloader_params.batch_size=$batch_size\
-                trainer.max_epochs=500\
-                trainer.min_epochs=50\
+                model.backbone.num_layers=1\
+                model.feature_encoder.out_channels=128\
+                model.feature_encoder.proj_dropout=0.25\
+                dataset.split_params.data_seed=0\
+                dataset.dataloader_params.batch_size=128\
+                trainer.max_epochs=5\
+                trainer.min_epochs=1\
                 trainer.devices=\[$CUDA\]\
-                trainer.check_val_every_n_epoch=5\
-                logger.wandb.project=$project_name\
-                optimizer.parameters.lr=$LEARNING_RATES_STR\
-                optimizer.parameters.weight_decay=$WEIGHT_DECAYS_STR\
+                trainer.check_val_every_n_epoch=1\
+                logger.wandb.project='prerun'\
+                optimizer.parameters.lr=0.01\
+                optimizer.parameters.weight_decay=0.25\
                 callbacks.early_stopping.patience=10\
                 transforms.graph2cell_lifting.max_cell_length=10\
+                transforms.sann_encoding.pe_types=[$pe_type]\
                 transforms.sann_encoding.neighborhoods=$neighborhood\
                 transforms.graph2cell_lifting.neighborhoods=$neighborhood\
                 --multirun &
+                sleep 5
         done
-    done
+        wait
+
+        gpus=(3 4)
+        for i in {0..1}; do 
+            CUDA=${gpus[$i]}  # Use the GPU number from our gpus array
+            neighborhood=${neighborhoods[$i]} # Use the neighbourhood from our neighbourhoods array
+
+            for batch_size in ${batch_sizes[*]}
+            do
+                for pd in ${PROJECTION_DROPOUTS[*]}
+                do
+                    python topobench/run.py\
+                        dataset=graph/$dataset\
+                        model=graph/$model_type\
+                        experiment=hopse_m_gnn_cell_zinc\
+                        model.backbone.num_layers=$N_LAYERS_STR\
+                        model.feature_encoder.out_channels=$OUT_CHANNELS_STR\
+                        model.feature_encoder.proj_dropout=$pd\
+                        seed=$DATA_SEEDS_STR\
+                        dataset.dataloader_params.batch_size=$batch_size\
+                        trainer.max_epochs=500\
+                        trainer.min_epochs=50\
+                        trainer.devices=\[$CUDA\]\
+                        trainer.check_val_every_n_epoch=5\
+                        logger.wandb.project=$project_name\
+                        optimizer.parameters.lr=$LEARNING_RATES_STR\
+                        optimizer.parameters.weight_decay=$WEIGHT_DECAYS_STR\
+                        callbacks.early_stopping.patience=10\
+                        transforms.graph2cell_lifting.max_cell_length=10\
+                        transforms.sann_encoding.pe_types=[$pe_type]\
+                        transforms.sann_encoding.neighborhoods=$neighborhood\
+                        transforms.graph2cell_lifting.neighborhoods=$neighborhood\
+                        --multirun &
+                done
+            done
+        done
+        wait
+done 
 done
-wait
