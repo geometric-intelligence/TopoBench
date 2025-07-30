99a6a0cf42ee2680191abf0ba5020df9c682fe59
Guillermo Bernardez <guille_gbg@hotmail.com>
2025-07-30 09:49:25 +0000

    Update env_setup.sh

--- env_setup.sh
+++ env_setup.sh
@@ -4,12 +4,12 @@ pip install -e '.[all]'
 
 # Note that not all combinations of torch and CUDA are available
 # See https://github.com/pyg-team/pyg-lib to check the configuration that works for you
-TORCH="2.3.0"   # available options: 2.0.0, 2.1.0, 2.2.0, 2.3.0, 2.4.0, ...
-CUDA="cpu"      # if available, select the CUDA version suitable for your system
+TORCH="2.6.0"   # preferably 2.4.0 or higher
+CUDA="cu124"    # if available, select the CUDA version suitable for your system
                 # available options: cpu, cu102, cu113, cu116, cu117, cu118, cu121, ...
 pip install torch==${TORCH} --extra-index-url https://download.pytorch.org/whl/${CUDA}
 pip install torch-scatter torch-sparse torch-cluster -f https://data.pyg.org/whl/torch-${TORCH}+${CUDA}.html
-pip install dgl -f https://data.dgl.ai/wheels/torch-${TORCH}/${CUDA}/repo.html
+pip install dgl -f https://data.dgl.ai/wheels/torch-2.4/${CUDA}/repo.html
 
 #pytest
 
