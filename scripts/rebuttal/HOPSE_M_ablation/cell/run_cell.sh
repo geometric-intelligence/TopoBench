4c5d9d4c8961838fdc210bdc7d558cbd03d6c886
levtelyatnikov <tlscabinet@gmail.com>
2025-08-04 14:20:21 -0700

    run_cell

---
+++ scripts/rebuttal/HOPSE_M_ablation/cell/run_cell.sh
@@ -0,0 +1,7 @@
+bash /home/levtel/projects/TopoBenchmark/scripts/rebuttal/HOPSE_M_ablation/cell/nci1.sh
+wait
+bash /home/levtel/projects/TopoBenchmark/scripts/rebuttal/HOPSE_M_ablation/cell/nci109.sh
+wait
+bash /home/levtel/projects/TopoBenchmark/scripts/rebuttal/HOPSE_M_ablation/cell/proteins.sh
+wait
+bash /home/levtel/projects/TopoBenchmark/scripts/rebuttal/HOPSE_M_ablation/cell/zinc.sh
