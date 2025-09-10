e818b4dded6f6cda95a94c334c9a6d736128ea4d
Guillermo Bernardez <guille_gbg@hotmail.com>
2025-09-10 00:46:09 +0000

    Fix minor bug in rich_utils

--- topobench/utils/rich_utils.py
+++ topobench/utils/rich_utils.py
@@ -20,7 +20,7 @@ log = pylogger.RankedLogger(__name__, rank_zero_only=True)
 def print_config_tree(
     cfg: DictConfig,
     print_order: Sequence[str] = (
-        "data",
+        "dataset",
         "model",
         "callbacks",
         "logger",
