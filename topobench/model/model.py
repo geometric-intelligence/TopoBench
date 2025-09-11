c82d73d0efcad0297fd219c7927042e09516a53b
Guillermo Bernardez <guille_gbg@hotmail.com>
2025-09-11 17:49:28 +0000

    Homophily experiments ready

--- topobench/model/model.py
+++ topobench/model/model.py
@@ -234,7 +234,7 @@ class TBModel(LightningModule):
         dict
             Dictionary containing the updated model output.
         """
-        if not self.task_level == "inductive_node":
+        if self.task_level == "node":
             # Get the correct mask
             if self.state_str == "Training":
                 mask = batch.train_mask
