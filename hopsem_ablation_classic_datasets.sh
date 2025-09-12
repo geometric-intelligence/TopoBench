757eddcfad6c5eff925c6eebce90d6175f5b725f
Guillermo <guille_gbg@hotmail.com>
2025-09-12 14:09:35 -0700

    Update scripts

--- hopsem_ablation_classic_datasets.sh
+++ hopsem_ablation_classic_datasets.sh
@@ -1,9 +1,2 @@
-bash /home/gbg141/TopoBenchmarkX/scripts/rebuttal/HOPSE_M_ablation/simplicial/mutag.sh
-wait
-bash /home/gbg141/TopoBenchmarkX/scripts/rebuttal/HOPSE_M_ablation/simplicial/nci1.sh
-wait
-bash /home/gbg141/TopoBenchmarkX/scripts/rebuttal/HOPSE_M_ablation/simplicial/nci109.sh
-wait
-bash /home/gbg141/TopoBenchmarkX/scripts/rebuttal/HOPSE_M_ablation/simplicial/proteins.sh
-wait
-bash /home/gbg141/TopoBenchmarkX/scripts/rebuttal/HOPSE_M_ablation/simplicial/zinc.sh
+
+bash /home/gbg141/TopoBench/scripts/rebuttal/HOPSE_M_ablation/simplicial/zinc.sh
