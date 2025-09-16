771294c25e7d8e66c394d4e4c19e5ea5f7f42307
Martin Carrasco <martin.carrasco@utec.edu.pe>
2025-09-16 22:10:46 +0200

    FIX: Graphs

--- scripts/rebuttal/molhiv/graph/gin_simplicial.sh
+++ scripts/rebuttal/molhiv/graph/gin_simplicial.sh
@@ -42,72 +42,63 @@ neighborhoods=(
     "['up_adjacency-0']"
 )
 
-gpus=(0 1 2 3 4 5 6 7)
-for i in {0..7}; do 
-    CUDA=${gpus[$i]}  # Use the GPU number from our gpus array
 
-    for pe_type in ${PE_TYPES[*]}
-    do 
+for pe_type in ${PE_TYPES[*]}
+do 
+    python topobench/run.py\
+        dataset=graph/$dataset\
+        model=graph/hopse_gin\
+        experiment=hopse_m_gnn_simplicial\
+        model.backbone.num_layers=1\
+        model.feature_encoder.out_channels=128\
+        model.feature_encoder.proj_dropout=0.1\
+        dataset.split_params.data_seed=0\
+        dataset.dataloader_params.batch_size=32\
+        trainer.max_epochs=5\
+        trainer.min_epochs=1\
+        trainer.devices=\[0\]\
+        trainer.check_val_every_n_epoch=1\
+        logger.wandb.project=$project_name\
+        optimizer.parameters.lr=0.001\
+        optimizer.parameters.weight_decay=0.0\
+        transforms.sann_encoding.pe_types=[$pe_type]\
+        transforms.sann_encoding.neighborhoods=${neighborhoods[0]}\
+        transforms.graph2simplicial_lifting.neighborhoods=${neighborhood[0]}\
+        model.feature_encoder.use_atom_encoder=True\
+        model.feature_encoder.use_bond_encoder=True\
+        --multirun
+        sleep 5
+done
+wait
+
+for pe_type in ${PE_TYPES[*]}
+do
+    for batch_size in ${batch_sizes}
+    do
+    
         python topobench/run.py\
             dataset=graph/$dataset\
             model=graph/hopse_gin\
             experiment=hopse_m_gnn_simplicial\
-            model.backbone.num_layers=1\
-            model.feature_encoder.out_channels=128\
-            model.feature_encoder.proj_dropout=0.1\
-            dataset.split_params.data_seed=0\
-            dataset.dataloader_params.batch_size=32\
-            trainer.max_epochs=5\
-            trainer.min_epochs=1\
-            trainer.devices=\[$CUDA\]\
-            trainer.check_val_every_n_epoch=1\
+            model.backbone.num_layers=$N_LAYERS_STR\
+            model.feature_encoder.out_channels=$OUT_CHANNELS_STR\
+            model.feature_encoder.proj_dropout=$PROJECTION_DROPOUTS_STR\
+            dataset.split_params.data_seed=$DATA_SEEDS_STR\
+            dataset.dataloader_params.batch_size=$batch_size\
+            trainer.max_epochs=100\
+            trainer.min_epochs=10\
+            trainer.devices=\[0\]\
+            trainer.check_val_every_n_epoch=5\
+            callbacks.early_stopping.patience=10\
             logger.wandb.project=$project_name\
-            optimizer.parameters.lr=0.001\
-            optimizer.parameters.weight_decay=0.0\
+            optimizer.parameters.lr=$LEARNING_RATES_STR\
+            optimizer.parameters.weight_decay=$WEIGHT_DECAYS_STR\
             transforms.sann_encoding.pe_types=[$pe_type]\
             transforms.sann_encoding.neighborhoods=${neighborhoods[0]}\
-            transforms.graph2simplicial_lifting.neighborhoods=${neighborhood[0]}\
+            transforms.graph2simplicial_lifting.neighborhoods=${neighborhoods[0]}\
             model.feature_encoder.use_atom_encoder=True\
             model.feature_encoder.use_bond_encoder=True\
             --multirun
-            sleep 5
-    done
-done
-wait
-
-gpus=(0 1 2 3 4 5 6 7)
-for i in {0..7}; do 
-    CUDA=${gpus[$i]}  # Use the GPU number from our gpus array
-
-    for pe_type in ${PE_TYPES[*]}
-    do
-        for batch_size in ${batch_sizes}
-        do
-        
-            python topobench/run.py\
-                dataset=graph/$dataset\
-                model=graph/hopse_gin\
-                experiment=hopse_m_gnn_simplicial\
-                model.backbone.num_layers=$N_LAYERS_STR\
-                model.feature_encoder.out_channels=$OUT_CHANNELS_STR\
-                model.feature_encoder.proj_dropout=$PROJECTION_DROPOUTS_STR\
-                dataset.split_params.data_seed=$DATA_SEEDS_STR\
-                dataset.dataloader_params.batch_size=$batch_size\
-                trainer.max_epochs=100\
-                trainer.min_epochs=10\
-                trainer.devices=\[$CUDA\]\
-                trainer.check_val_every_n_epoch=5\
-                callbacks.early_stopping.patience=10\
-                logger.wandb.project=$project_name\
-                optimizer.parameters.lr=$LEARNING_RATES_STR\
-                optimizer.parameters.weight_decay=$WEIGHT_DECAYS_STR\
-                transforms.sann_encoding.pe_types=[$pe_type]\
-                transforms.sann_encoding.neighborhoods=${neighborhoods[0]}\
-                transforms.graph2simplicial_lifting.neighborhoods=${neighborhoods[0]}\
-                model.feature_encoder.use_atom_encoder=True\
-                model.feature_encoder.use_bond_encoder=True\
-                --multirun
-        done
     done
 done
 wait
--- scripts/rebuttal/molhiv/graph/solo_graph.sh
+++ scripts/rebuttal/molhiv/graph/solo_graph.sh
@@ -35,38 +35,32 @@ BATCH_SIZES_STR=$(IFS=,; echo "${BATCH_SIZES[*]}")
 
 # =====================
 
-gpus=(0 1 2 3 4 5 6 7)
-for i in {0..4}; do 
-    CUDA=${gpus[$i]}  # Use the GPU number from our gpus array
-    data_seed=${DATA_SEEDS[$i]} # Use the neighbourhood from our neighbourhoods array
     
-    for lr in ${LEARNING_RATES[*]}
+for lr in ${LEARNING_RATES[*]}
+do
+    for batch_size in ${BATCH_SIZES[*]}
     do
-        for batch_size in ${BATCH_SIZES[*]}
-        do
-            python topobench/run.py\
-                dataset=graph/$dataset\
-                model=graph/gin\
-                model.backbone.num_layers=$N_LAYERS_STR\
-                model.feature_encoder.out_channels=$OUT_CHANNELS_STR\
-                model.feature_encoder.proj_dropout=$PROJECTION_DROPOUTS_STR\
-                dataset.split_params.data_seed=$data_seed\
-                dataset.dataloader_params.batch_size=$batch_size\
-                trainer.devices=\[$CUDA\]\
-                trainer.max_epochs=100\
-                trainer.min_epochs=10\
-                trainer.check_val_every_n_epoch=5\
-                callbacks.early_stopping.patience=10\
-                optimizer.parameters.lr=$lr\
-                optimizer.parameters.weight_decay=$WEIGHT_DECAYS_STR\
-                transforms=no_transform\
-                logger.wandb.project=$project_name\
-                --multirun &
-            sleep 10
-        done
+        python topobench/run.py\
+            dataset=graph/$dataset\
+            model=graph/gin\
+            model.backbone.num_layers=$N_LAYERS_STR\
+            model.feature_encoder.out_channels=$OUT_CHANNELS_STR\
+            model.feature_encoder.proj_dropout=$PROJECTION_DROPOUTS_STR\
+            dataset.split_params.data_seed=$data_seed\
+            dataset.dataloader_params.batch_size=$batch_size\
+            trainer.devices=\[0\]\
+            trainer.max_epochs=100\
+            trainer.min_epochs=10\
+            trainer.check_val_every_n_epoch=5\
+            callbacks.early_stopping.patience=10\
+            optimizer.parameters.lr=$lr\
+            optimizer.parameters.weight_decay=$WEIGHT_DECAYS_STR\
+            transforms=no_transform\
+            logger.wandb.project=$project_name\
+            --multirun &
+        sleep 10
     done
 done
-wait
 
 
 
