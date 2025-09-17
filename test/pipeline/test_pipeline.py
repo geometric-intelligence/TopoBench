955bb66163062850e63daad41e055fddc54b5576
Guillermo Bernardez <guille_gbg@hotmail.com>
2025-09-17 20:21:58 +0000

    Update test_pipeline

--- test/pipeline/test_pipeline.py
+++ test/pipeline/test_pipeline.py
@@ -4,8 +4,8 @@ import hydra
 from test._utils.simplified_pipeline import run
 
 
-DATASET = "graph/MUTAG"     # ADD YOUR DATASET HERE
-MODEL   = "graph/gcn"       # ADD A MODEL OF YOUR CHOICE HERE
+DATASET = "graph/MUTAG"                                                 # ADD YOUR DATASET HERE
+MODELS   = ["graph/gcn", "cell/topotune", "simplicial/topotune"]        # ADD ONE OR SEVERAL MODELS OF YOUR CHOICE HERE
 
 
 class TestPipeline:
@@ -18,17 +18,18 @@ class TestPipeline:
     def test_pipeline(self):
         """Test pipeline."""
         with hydra.initialize(config_path="../../configs", job_name="job"):
-            cfg = hydra.compose(
-                config_name="run.yaml",
-                overrides=[
-                    f"model={MODEL}",
-                    f"dataset={DATASET}", # IF YOU IMPLEMENT A LARGE DATASET WITH AN OPTION TO USE A SLICE OF IT, ADD BELOW THE CORRESPONDING OPTION
-                    "trainer.max_epochs=2",
-                    "trainer.min_epochs=1",
-                    "trainer.check_val_every_n_epoch=1",
-                    "paths=test",
-                    "callbacks=model_checkpoint",
-                ],
-                return_hydra_config=True
-            )
-            run(cfg)
+            for MODEL in MODELS:
+                cfg = hydra.compose(
+                    config_name="run.yaml",
+                    overrides=[
+                        f"model={MODEL}",
+                        f"dataset={DATASET}", # IF YOU IMPLEMENT A LARGE DATASET WITH AN OPTION TO USE A SLICE OF IT, ADD BELOW THE CORRESPONDING OPTION
+                        "trainer.max_epochs=2",
+                        "trainer.min_epochs=1",
+                        "trainer.check_val_every_n_epoch=1",
+                        "paths=test",
+                        "callbacks=model_checkpoint",
+                    ],
+                    return_hydra_config=True
+                )
+                run(cfg)
