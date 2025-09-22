0b102992f30cb98f8a4789a6470badc12116a6bc
guille <guille_gbg@hotmail.com>
2025-09-22 16:36:55 +0000

    Final triangle counting experiments

---
+++ scripts/graphuniverse_topotune_triangle.sh
@@ -0,0 +1,112 @@
+#!/bin/bash
+
+
+data_seeds=(1 3 5)
+
+for i in ${data_seeds[@]}; do
+    python -m topobench \
+        dataset=graph/GraphUniverse_TC \
+        dataset.loader.parameters.generation_parameters.universe_parameters.seed=$i \
+        dataset.loader.parameters.generation_parameters.family_parameters.degree_separation_range=\[0.0,0.1\],\[0.9,1.0\] \
+        dataset.loader.parameters.generation_parameters.family_parameters.n_graphs=1000 \
+        model=cell/topotune \
+        model.feature_encoder.out_channels=64 \
+        model.feature_encoder.proj_dropout=0.3 \
+        model.tune_gnn=GCN,GIN,GAT,GraphSAGE \
+        model.backbone.GNN.num_layers=1 \
+        model.backbone.neighborhoods=\[1-up_laplacian-0,1-up_laplacian-1\],\[1-up_laplacian-0,1-down_laplacian-2\],\[1-up_laplacian-0,1-down_incidence-2\],\[1-up_laplacian-0,2-down_laplacian-2\],\[1-up_laplacian-0,1-up_incidence-0,1-up_laplacian-1,1-up_incidence-1\] \
+        model.backbone.layers=2,4 \
+        model.readout.readout_name=PropagateSignalDown \
+        model.readout.pooling_type=mean,sum \
+        dataset.split_params.data_seed=$i \
+        dataset.dataloader_params.batch_size=32 \
+        logger.wandb.project=final_triangle_experiments \
+        trainer.max_epochs=1000 \
+        trainer.min_epochs=50 \
+        trainer.devices=\[0\] \
+        trainer.check_val_every_n_epoch=1 \
+        callbacks.early_stopping.patience=50 \
+        tags="[triangle]" \
+        --multirun &
+
+    sleep 100
+    
+    python -m topobench \
+        dataset=graph/GraphUniverse_TC \
+        dataset.loader.parameters.generation_parameters.universe_parameters.seed=$i \
+        dataset.loader.parameters.generation_parameters.family_parameters.degree_separation_range=\[0.0,0.1\],\[0.9,1.0\] \
+        dataset.loader.parameters.generation_parameters.family_parameters.n_graphs=1000 \
+        model=cell/topotune \
+        model.feature_encoder.out_channels=64 \
+        model.feature_encoder.proj_dropout=0.3 \
+        model.tune_gnn=GCN,GIN,GAT,GraphSAGE \
+        model.backbone.GNN.num_layers=1 \
+        model.backbone.neighborhoods=\[1-up_laplacian-0,1-up_laplacian-1\],\[1-up_laplacian-0,1-down_laplacian-2\],\[1-up_laplacian-0,1-down_incidence-2\],\[1-up_laplacian-0,2-down_laplacian-2\],\[1-up_laplacian-0,1-up_incidence-0,1-up_laplacian-1,1-up_incidence-1\] \
+        model.backbone.layers=2,4 \
+        model.readout.readout_name=MLPReadout \
+        model.readout.hidden_layers=\[16\],\[\] \
+        model.readout.dropout=0.3 \
+        dataset.split_params.data_seed=$i \
+        dataset.dataloader_params.batch_size=32 \
+        logger.wandb.project=final_triangle_experiments \
+        trainer.max_epochs=1000 \
+        trainer.min_epochs=50 \
+        trainer.devices=\[1\] \
+        trainer.check_val_every_n_epoch=1 \
+        callbacks.early_stopping.patience=50 \
+        tags="[triangle]" \
+        --multirun &
+
+    python -m topobench \
+        dataset=graph/GraphUniverse_TC \
+        dataset.loader.parameters.generation_parameters.universe_parameters.seed=$i \
+        dataset.loader.parameters.generation_parameters.family_parameters.degree_separation_range=\[0.0,0.1\],\[0.9,1.0\] \
+        dataset.loader.parameters.generation_parameters.family_parameters.n_graphs=1000 \
+        model=simplicial/topotune \
+        model.feature_encoder.out_channels=64 \
+        model.feature_encoder.proj_dropout=0.3 \
+        model.tune_gnn=GCN,GIN,GAT,GraphSAGE \
+        model.backbone.GNN.num_layers=1 \
+        model.backbone.neighborhoods=\[1-up_laplacian-0,1-up_laplacian-1\],\[1-up_laplacian-0,1-down_laplacian-2\],\[1-up_laplacian-0,1-down_incidence-2\],\[1-up_laplacian-0,2-down_laplacian-2\],\[1-up_laplacian-0,1-up_incidence-0,1-up_laplacian-1,1-up_incidence-1\] \
+        model.backbone.layers=2,4 \
+        model.readout.readout_name=PropagateSignalDown \
+        model.readout.pooling_type=mean,sum \
+        dataset.split_params.data_seed=$i \
+        dataset.dataloader_params.batch_size=32 \
+        logger.wandb.project=final_triangle_experiments \
+        trainer.max_epochs=1000 \
+        trainer.min_epochs=50 \
+        trainer.devices=\[2\] \
+        trainer.check_val_every_n_epoch=1 \
+        callbacks.early_stopping.patience=50 \
+        tags="[triangle]" \
+        --multirun &
+
+    sleep 100
+    
+    python -m topobench \
+        dataset=graph/GraphUniverse_TC \
+        dataset.loader.parameters.generation_parameters.universe_parameters.seed=$i \
+        dataset.loader.parameters.generation_parameters.family_parameters.degree_separation_range=\[0.0,0.1\],\[0.9,1.0\] \
+        dataset.loader.parameters.generation_parameters.family_parameters.n_graphs=1000 \
+        model=simplicial/topotune \
+        model.feature_encoder.out_channels=64 \
+        model.feature_encoder.proj_dropout=0.3 \
+        model.tune_gnn=GCN,GIN,GAT,GraphSAGE \
+        model.backbone.GNN.num_layers=1 \
+        model.backbone.neighborhoods=\[1-up_laplacian-0,1-up_laplacian-1\],\[1-up_laplacian-0,1-down_laplacian-2\],\[1-up_laplacian-0,1-down_incidence-2\],\[1-up_laplacian-0,2-down_laplacian-2\],\[1-up_laplacian-0,1-up_incidence-0,1-up_laplacian-1,1-up_incidence-1\] \
+        model.backbone.layers=2,4 \
+        model.readout.readout_name=MLPReadout \
+        model.readout.hidden_layers=\[16\],\[\] \
+        model.readout.dropout=0.3 \
+        dataset.split_params.data_seed=$i \
+        dataset.dataloader_params.batch_size=32 \
+        logger.wandb.project=final_triangle_experiments \
+        trainer.max_epochs=1000 \
+        trainer.min_epochs=50 \
+        trainer.devices=\[3\] \
+        trainer.check_val_every_n_epoch=1 \
+        callbacks.early_stopping.patience=50 \
+        tags="[triangle]" \
+        --multirun &
+done
