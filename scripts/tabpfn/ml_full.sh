292bf26f939d18b89b18d2ecf488984a74e6cbee
Loris697 <loriscino97@gmail.com>
2025-10-23 18:33:22 +0200

    Adding scripts

---
+++ scripts/tabpfn/ml_full.sh
@@ -0,0 +1,107 @@
+#!/bin/bash
+# ======================
+# USER CONFIGURATION
+# ======================
+project_name='ml_on_full_graph_extended'
+exp_name="ml"   # Set your experiment name here
+
+# --- Classification (binary) ---
+
+datasets_classification_binary=(
+#  "graph/tolokers"
+#  "graph/tolokers-2"
+#  "graph/minesweeper"
+#  "graph/questions"
+#  "graph/web-fraud"
+  "graph/artnet-exp",
+  "graph/city-reviews"
+)
+
+# --- Classification (multiclass) ---
+#datasets_classification_multiclass=(
+#  "graph/roman_empire"
+#  "graph/amazon_ratings"
+#  "graph/cocitation_citeseer"
+#  "graph/cocitation_cora"
+#  "graph/cocitation_pubmed"
+#  "graph/hm-categories"
+#  "graph/pokec-regions"
+#  "graph/web-topics"
+#  "graph/wiki_cs"
+#)
+
+datasets_regression=(
+#  "graph/artnet-views"
+#  "graph/avazu-ctr"
+#  "graph/city-roads-L"
+#  "graph/city-roads-M"
+#  "graph/hm-prices"
+  "graph/web-traffic"
+  "graph/twitch-views"
+#  "graph/US-BachelorRate"
+#  "graph/US-BirthRate"
+#  "graph/US-DeathRate"
+#  "graph/US-Election"
+#  "graph/US-MedianIncome"
+#  "graph/US-MigraRate"
+#  "graph/US-UnemploymentRate"
+)
+
+models_classification=("dt_c" "hgb_c" "lgbm_c" "logistic_regression" "mlp_c" "xgb_c", "rf_c")
+models_regression=("rf_r" "dt_r" "hgb_r" "lgbm_r" "linear_regression" "mlp_r" "xgb_r")
+seeds="0,1,2,3,4"
+
+# ---- CLASSIFICATION: BINARY ----
+for d in "${datasets_classification_binary[@]}"; do
+  for m in "${models_classification[@]}"; do
+    echo "Running BINARY classification dataset=$d model=$m"
+    python -m topobench -m \
+      dataset=$d \
+      evaluator=classification_extended \
+      model=non_relational/sklearn_classifier \
+      model/non_relational/sklearn@model.backbone=$m \
+      model/non_relational/sklearn/samplers@model.backbone_wrapper.sampler=none \
+      train=False \
+      dataset.split_params.data_seed=$seeds \
+      logger.wandb.project=$project_name \
+      trainer=cpu \
+      loss=no_loss
+  done
+done
+
+# ---- CLASSIFICATION: MULTICLASS ----
+for d in "${datasets_classification_multiclass[@]}"; do
+  for m in "${models_classification[@]}"; do
+    echo "Running MULTICLASS classification dataset=$d model=$m"
+    python -m topobench -m \
+      dataset=$d \
+      evaluator=classification_extended \
+      model=non_relational/sklearn_classifier \
+      model/non_relational/sklearn@model.backbone=$m \
+      model/non_relational/sklearn/samplers@model.backbone_wrapper.sampler=none \
+      train=False \
+      dataset.split_params.data_seed=$seeds \
+      logger.wandb.project=$project_name \
+      trainer=cpu \
+      loss=no_loss
+  done
+done
+
+# ---- REGRESSION ----
+for d in "${datasets_regression[@]}"; do
+  for m in "${models_regression[@]}"; do
+    echo "Running REGRESSION dataset=$d model=$m"
+    python -m topobench -m \
+      dataset=$d \
+      evaluator=regression_extended \
+      model=non_relational/sklearn_regressor \
+      model/non_relational/sklearn@model.backbone=$m \
+      model/non_relational/sklearn/samplers@model.backbone_wrapper.sampler=none \
+      train=False \
+      dataset.split_params.data_seed=$seeds \
+      logger.wandb.project=$project_name \
+      trainer=cpu \
+      loss=no_loss
+  done
+done
+
