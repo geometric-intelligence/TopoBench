19a19066d1754b8a6bb84446797d2c7b6dffab5f
jonas <jonas.c.r.mueller@gmail.com>
2025-11-07 00:37:05 +0100

    added test and some fixes

--- test/pipeline/test_pipeline.py
+++ test/pipeline/test_pipeline.py
@@ -4,7 +4,7 @@ import hydra
 from test._utils.simplified_pipeline import run
 
 
-DATASET = "graph/twitch"                                                 # ADD YOUR DATASET HERE
+DATASET = "graph/twitch"                                            # ADD YOUR DATASET HERE
 MODELS   = ["graph/gcn"]        # ADD ONE OR SEVERAL MODELS OF YOUR CHOICE HERE
 
 
