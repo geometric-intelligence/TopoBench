b93c0e150f93aa035bba03eaadd630929bfd554d
Patrick Liu <patrick_liu@ucsb.edu>
2025-11-16 11:22:38 -0800

    Removed Txt Files, Changed YAML Params, Changed pathing

--- test/pipeline/test_pipeline.py
+++ test/pipeline/test_pipeline.py
@@ -4,8 +4,8 @@ import hydra
 from test._utils.simplified_pipeline import run
 
 
-DATASET = "hypergraph/temporal/DAWN"                                                # ADD YOUR DATASET HERE
-MODELS   = ["graph/gcn", "cell/topotune", "simplicial/topotune"]        # ADD ONE OR SEVERAL MODELS OF YOUR CHOICE HERE
+DATASET = "hypergraph/dawn"                                                # ADD YOUR DATASET HERE
+MODELS   = ["graph/gcn"]        # ADD ONE OR SEVERAL MODELS OF YOUR CHOICE HERE
 
 
 class TestPipeline:
