675496e876a318b2ad389cc21dcedc4d93136e63
Loris697 <loriscino97@gmail.com>
2025-12-18 18:57:35 +0100

    Adding random sampler

--- topobench/nn/wrappers/graph/sklearn/sklearn_wrappers/samplers/__init__.py
+++ topobench/nn/wrappers/graph/sklearn/sklearn_wrappers/samplers/__init__.py
@@ -2,3 +2,4 @@ from .base_sampler import BaseSampler
 from.composite_sampler import CompositeSampler
 from .knn_sampler import KNNSampler
 from .graph_sampler import GraphHopSampler
+from .random_sampler import RandomSampler
