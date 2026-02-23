23c6e6e9f39060daebbaa454e897d07ebf9e1945
Louis Van Langendonck <74510892+LouisVanLangendonck@users.noreply.github.com>
2026-02-23 19:32:21 +0100

    Add preprocess progress checking

--- topobench/data/preprocessor/preprocessor.py
+++ topobench/data/preprocessor/preprocessor.py
@@ -8,6 +8,7 @@ import torch
 import torch_geometric
 from torch_geometric.io import fs
 from omegaconf import OmegaConf
+from tqdm import tqdm
 from topobench.data.utils import (
     ensure_serializable,
     load_inductive_splits,
@@ -193,11 +194,15 @@ class PreProcessor(torch_geometric.data.InMemoryDataset):
         elif isinstance(self.dataset, torch_geometric.data.Data):
             data_list = [self.dataset]
 
-        self.data_list = (
-            [self.pre_transform(d) for d in data_list]
-            if self.pre_transform is not None
-            else data_list
-        )
+        if self.pre_transform is not None:
+            print(f"\nApplying transforms to {len(data_list)} graphs...")
+            self.data_list = [
+                self.pre_transform(d) 
+                for d in tqdm(data_list, desc="Processing graphs", unit="graph")
+            ]
+        else:
+            self.data_list = data_list
+            
         self._data, self.slices = self.collate(self.data_list)
         self._data_list = None  # Reset cache.
 
