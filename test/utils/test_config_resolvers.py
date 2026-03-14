b49226aaab6cd6fa045b3a31825fe5fc38418c12
Guillermo Bernardez <guille_gbg@hotmail.com>
2026-03-14 01:43:02 +0000

    Fix bug in infer_in_channels (Conbcatenation)

--- test/utils/test_config_resolvers.py
+++ test/utils/test_config_resolvers.py
@@ -128,7 +128,7 @@ class TestConfigResolvers:
         
         cfg = hydra.compose(config_name="run.yaml", overrides=["model=simplicial/topotune", "dataset=graph/MUTAG", "dataset.parameters.preserve_edge_attr_if_lifted=False", "transforms.graph2simplicial_lifting.feature_lifting=Concatenation"], return_hydra_config=True)
         in_channels = infer_in_channels(cfg.dataset, cfg.transforms)
-        assert in_channels == [7,14,42]
+        assert in_channels == [7,14,42,168]
         
         cfg = hydra.compose(config_name="run.yaml", overrides=["model=simplicial/topotune", "dataset=graph/MUTAG", "transforms.graph2simplicial_lifting.feature_lifting=Concatenation"], return_hydra_config=True)
         in_channels = infer_in_channels(cfg.dataset, cfg.transforms)
@@ -136,7 +136,7 @@ class TestConfigResolvers:
         
         cfg = hydra.compose(config_name="run.yaml", overrides=["model=simplicial/topotune", "dataset=graph/cocitation_cora", "transforms.graph2simplicial_lifting.feature_lifting=Concatenation"], return_hydra_config=True)
         in_channels = infer_in_channels(cfg.dataset, cfg.transforms)
-        assert in_channels == [1433,2866,8598]
+        assert in_channels == [1433,2866,8598,34392]
         
         cfg = hydra.compose(config_name="run.yaml", overrides=["model=simplicial/topotune", "dataset=graph/cocitation_cora"], return_hydra_config=True)
         in_channels = infer_in_channels(cfg.dataset, cfg.transforms)
